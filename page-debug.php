e1f253707fa3a85d5bdacef3c888aa717be66682
Erich Richter <info@ErichRichter.com>
2020-01-06 21:28:08 -0800

    New page, for debugging code.
    This debug page replaces the old cron-test page. It is more generic and simply allows a programmer a test page to experiment plugin output. Handy for debugging silent AJAX calls with JSON output.

---
+++ page-debug.php
@@ -0,0 +1,10 @@
+<!-- Use this page to debug things that are difficult to debug like JSON output. -->
+<div id="artlogic" class="center page-admin">
+<h4>Debug</h4>
+
+<?php
+	$cron = false;
+	$artlogic->sync($cron);
+?>
+
+</div>
